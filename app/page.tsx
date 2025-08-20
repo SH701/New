2698cdf1768c1e0dd27eefe2ddb16001180026d9
SH701 <rkrk7636@naver.com>
2025-08-20 16:31:07 +0900

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="relative pt-5 w-screen h-screen">
-      <Image src="/www.png" alt="배경" fill className="object-cover -z-10" />
+      <Image src="/wrap.png" alt="배경" fill className="object-cover -z-10" />
       <div className="w-[375px] h-[812px] z-10 absolute right-50 top-10">
         <EmbedPage url="https://noonchi.ai.kr" />
       </div>
